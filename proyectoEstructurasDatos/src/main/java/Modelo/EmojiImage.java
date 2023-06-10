69ca6bb6d62fb2a772443ed3f4e4cb209a8d38a4
DiegoC <diealcon@espol.edu.ec>
2023-06-10 18:39:32 -0500

    EmojiImage Class/Rename LinkedList.

---
+++ proyectoEstructurasDatos/src/main/java/Modelo/EmojiImage.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Win1OPro Station
+ */
+public class EmojiImage {
+    private String name;
+    private String imagerute;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImagerute() {
+        return imagerute;
+    }
+
+    public void setImagerute(String imagerute) {
+        this.imagerute = imagerute;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
