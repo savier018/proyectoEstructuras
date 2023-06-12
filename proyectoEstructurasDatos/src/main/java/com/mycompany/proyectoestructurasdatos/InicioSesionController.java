0edaa5ccab20a0618598cbf88e3a8336b4cd2568
fnand <fnand@192.168.1.118>
2023-06-12 16:52:04 -0500

    Agregado pantalla usuario

--- proyectoEstructurasDatos/src/main/java/com/mycompany/proyectoestructurasdatos/InicioSesionController.java
+++ proyectoEstructurasDatos/src/main/java/com/mycompany/proyectoestructurasdatos/InicioSesionController.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.proyectoestructurasdatos;
 
 import java.io.IOException;
@@ -6,9 +5,7 @@ import java.net.URL;
 import TDA.ArrayList;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -43,14 +40,12 @@ public class InicioSesionController implements Initializable {
         String user = usuario.getText();
         String pass = clave.getText();
         int cont=0; //va a sumar los intentos correctos, verificando que la informacion ingresada es correcta
-        /*for(int i= 0;i<usuarioLista.size(); i++){ //recorre la lista usuario en donde si pertence a un nivel lo lleve a la interfaz de ese
+        for(int i= 0;i<usuarioLista.size(); i++){ //recorre la lista usuario en donde si pertence a un nivel lo lleve a la interfaz de ese
             Usuario u = usuarioLista.get(i); 
             if(user.equals(u.getUsuario()) && pass.equals(u.getContraseña())){
                     //PantallaUsuario.setNombreUsuario(user);
-                    try {
-                        FXMLLoader loader = new FXMLLoader(App.class.getResource("pantallaUsuario.fxml"));
-                        Parent vistaAdmin = loader.load();
-                        App.setRoot(vistaAdmin);
+                    try {                   
+                        App.setRoot("pantallaUsuario");
                     } 
                     catch (IOException ex) {
                         iniciarSesion.setDisable(true);
@@ -67,9 +62,9 @@ public class InicioSesionController implements Initializable {
                 alert.setContentText("Su usuario o contraseña son erroneas, intentelo de nuevo");
 
                 alert.showAndWait();
-                App.setRoot("iniciaSesion");
+                App.setRoot("inicioSesion");
                 alert.close();
-        }*/
+        }
     }
 
 }
--- proyectoEstructurasDatos/src/main/java/com/mycompany/proyectoestructurasdatos/PantallaUsuarioController.java
+++ proyectoEstructurasDatos/src/main/java/com/mycompany/proyectoestructurasdatos/PantallaUsuarioController.java
@@ -1,12 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
+
+
 package com.mycompany.proyectoestructurasdatos;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import java.io.IOException;
+import java.net.URL;
+import TDA.ArrayList;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import Modelo.Usuario;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
@@ -14,7 +23,15 @@ import javafx.fxml.Initializable;
  * @author fnand
  */
 public class PantallaUsuarioController implements Initializable {
-
+    @FXML
+    private TextField textNombre;
+    
+    static String nombreUsuario;
+    
+    @FXML
+    private Button nuevo;
+    @FXML
+    private Button seguir;
     /**
      * Initializes the controller class.
      */
@@ -22,5 +39,16 @@ public class PantallaUsuarioController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    @FXML
+    public void nuevo()throws IOException {
+        // TODO
+    }   
+     
+    @FXML
+    public void seguir()throws IOException {
+        // TODO
+    }   
+    
     
 }
